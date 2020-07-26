25c15d004083e8a91ee586868fb4e6cf009d1de9
Sophie Alexandre <55605315+sophiealexandre@users.noreply.github.com>
2020-07-26 19:47:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-fetch("https://www.hatchways.io/api/assessment/work_orders")
-      .then(response => response.json())
-      .then((data) => {
+ fetch("https://www.hatchways.io/api/assessment/work_orders")
+     .then(response => response.json())
+     .then((data) => {
 
-      var deadlines = data.orders.map(order => order.deadline);
-      var descriptions = data.orders.map(order => order.description);
-      var ids = data.orders.map(order => order.id);
-      var names = data.orders.map(order => order.name);
-      var workerIds = data.orders.map(order => order.workerId);
-      console.log(deadlines)
-      //for (let i = 0; i < ids.length; i++)
-      //console.log(deadlines[i])
+         var deadlines = data.orders.map(order => order.deadline);
+         var descriptions = data.orders.map(order => order.description);
+         var ids = data.orders.map(order => order.id);
+         var names = data.orders.map(order => order.name);
+         var workerIds = data.orders.map(order => order.workerId);
+         console.log(deadlines)
+         //for (let i = 0; i < ids.length; i++)
+         //console.log(deadlines[i])
 
-      var uniqueWorkerIds = workerIds.filter(function(item, pos){
-        return workerIds.indexOf(item)== pos;
-      });
-      console.log(uniqueWorkerIds );
-      for (let i = 0; i < uniqueWorkerIds; i++)
+         var uniqueWorkerIds = workerIds.filter(function(item, pos) {
+             return workerIds.indexOf(item) == pos;
+         });
+         console.log(uniqueWorkerIds);
+         for (let i = 0; i < uniqueWorkerIds; i++) {
+             fetch("https://www.hatchways.io/api/assessment/workers/" + i)
+                 .then(response => response.json())
+                 .then((data) => {
+                     var emails = data.workers.map(worker => worker.deadline);
+                     console.log(emails)
+                 });
 
-      fetch("https://www.hatchways.io/api/assessment/workers/" + i)
-      .then(response => response.json())
-      .then((data) => {
-      var email = data.worker[i].email
-      console.log(email)
-  })
+         };
 
 
-  });
 
+     });
